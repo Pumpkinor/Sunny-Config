afc60d5e2b4c50dceb6182cee48558c6c7b1dd31
pumpkinor <pumpkinor@163.com>
2024-07-07 16:49:30 +0800

    #change use Ordered.HIGHEST_PRECEDENCE to define bean load order and change some comments

--- sunny-config-client/src/main/java/com/sunny/sunnyconfigclient/config/PropertySourceProcessor.java
+++ sunny-config-client/src/main/java/com/sunny/sunnyconfigclient/config/PropertySourceProcessor.java
@@ -1,6 +1,5 @@
 package com.sunny.sunnyconfigclient.config;
 
-import com.sunny.sunnyconfigclient.config.SunnyPropertySource;
 import com.sunny.sunnyconfigclient.service.SunnyConfigService;
 import com.sunny.sunnyconfigclient.service.SunnyConfigServiceImpl;
 import org.jetbrains.annotations.NotNull;
@@ -8,6 +7,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.EnvironmentAware;
+import org.springframework.core.Ordered;
 import org.springframework.core.PriorityOrdered;
 import org.springframework.core.env.CompositePropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -24,6 +24,8 @@ public class PropertySourceProcessor implements BeanFactoryPostProcessor, Enviro
     Environment environment;
     @Override
     public void postProcessBeanFactory(@NotNull final ConfigurableListableBeanFactory beanFactory) throws BeansException {
+//        TODO 其实实现这个BeanFactoryPostProcessor接口重写postProcessBeanFactory方法好像意义不大 这部分逻辑写到其他位置也都ok
+//        毕竟所有操作都是基于environment对象来的 
         ConfigurableEnvironment configurableEnvironment = (ConfigurableEnvironment) environment;
         MutablePropertySources propertySources = configurableEnvironment.getPropertySources();
         if (propertySources.contains(SUNNY_PROPERTY_SOURCES)){
@@ -49,6 +51,6 @@ public class PropertySourceProcessor implements BeanFactoryPostProcessor, Enviro
     
     @Override
     public int getOrder() {
-        return 0;
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 }
