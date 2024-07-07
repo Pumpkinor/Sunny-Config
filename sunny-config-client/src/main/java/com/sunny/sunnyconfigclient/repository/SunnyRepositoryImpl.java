e5c0ef9eb3e22e848ff9b02346119fd995948fa7
pumpkinor <pumpkinor@163.com>
2024-07-07 18:18:42 +0800

    #add fix time to pull configs from config-server

--- sunny-config-client/src/main/java/com/sunny/sunnyconfigclient/repository/SunnyRepositoryImpl.java
+++ sunny-config-client/src/main/java/com/sunny/sunnyconfigclient/repository/SunnyRepositoryImpl.java
@@ -4,23 +4,58 @@ import cn.kimmking.utils.HttpUtils;
 import com.alibaba.fastjson.TypeReference;
 import com.sunny.sunnyconfigclient.model.ConfigMeta;
 import com.sunny.sunnyconfigclient.model.Configs;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
  * default implement of SunnyRepository
  */
-
+@Slf4j
 public class SunnyRepositoryImpl implements SunnyRepository{
     private ConfigMeta configMeta;
+    
+    private Map<String ,Long> configsVersion = new HashMap<>();
+    
+    private Map<String, Map<String, String>> configs = new HashMap<>();
+    
+    ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+    
     public SunnyRepositoryImpl(final ConfigMeta configMeta) {
         this.configMeta = configMeta;
+        executorService.scheduleWithFixedDelay(this::heartBeat, 0, 1000, TimeUnit.MICROSECONDS);
+    }
+    
+    private void heartBeat() {
+        String key = configMeta.genKey();
+        Long oldVersion = configsVersion.getOrDefault(key,-1L);
+        Long version = HttpUtils.httpGet(configMeta.versionPath(), new TypeReference<>() {});
+        if (version != null && version > oldVersion){
+            configsVersion.put(configMeta.genKey(), version);
+            log.info("configs changed, version:{}", version);
+            configs.put(key,findAll());
+            log.info("configs:{}", configs.get(key));
+        }
     }
     
     @Override
-    public Map<String, String> getConfig() {         
+    public Map<String, String> getConfig() {     
+        String key = configMeta.genKey();
+        if (configs.containsKey(key)){
+            return configs.get(key);
+        }
+        return findAll();
+    }
+    
+    @NotNull
+    private Map<String, String> findAll() {
         final List<Configs> configs = HttpUtils.httpGet(configMeta.listPath(), new TypeReference<>() {});
         return configs.stream().collect(Collectors.toMap(Configs::getPkey, Configs::getPval, (oldValue, newValue) -> newValue));
     }
